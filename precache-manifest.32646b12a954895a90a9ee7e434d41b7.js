520b425222e9f16815984f0c478f649960c04ec9
Ivan Sidorenko <i.sidorenko@plarium.com>
2019-09-11 14:35:06 +0300

    Updates

---
+++ precache-manifest.32646b12a954895a90a9ee7e434d41b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34d38556ed52c37794e77f69122ccedb",
+    "url": "/sport-events/index.html"
+  },
+  {
+    "revision": "af10304a00dcee5a1268",
+    "url": "/sport-events/static/css/main.9922e437.chunk.css"
+  },
+  {
+    "revision": "89d7f300efcdf2a0c5a2",
+    "url": "/sport-events/static/js/2.5a5e869b.chunk.js"
+  },
+  {
+    "revision": "af10304a00dcee5a1268",
+    "url": "/sport-events/static/js/main.f2cd268c.chunk.js"
+  },
+  {
+    "revision": "4f9d3ced48d506e39d13",
+    "url": "/sport-events/static/js/runtime~main.a2ab4f5d.js"
+  }
+]);
